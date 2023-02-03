3bff1c1eab55a70d568c463e40234fff049e6fec
Joram van den Boezem <joram@vandenboezem.nl>
2023-02-03 13:08:06 +0100

    chore: fix tests

--- tests/models/personenauto.test.ts
+++ tests/models/personenauto.test.ts
@@ -1,81 +1,77 @@
-import {
-  findTarief,
-  Model_Personenauto,
-  rondGewichtAf,
-} from "../../src/models/personenauto.js";
+import { Model_Personenauto } from "../../src/models/personenauto.js";
 import { Brandstof } from "../../src/params.js";
 
-test("rondGewichtAf", () => {
-  expect(rondGewichtAf(0)).toBe(0);
-  expect(rondGewichtAf(510)).toBe(500);
-  expect(rondGewichtAf(550)).toBe(500);
-  expect(rondGewichtAf(560)).toBe(600);
-  expect(rondGewichtAf(1050)).toBe(1000);
-  expect(rondGewichtAf(1051)).toBe(1100);
-});
+// test("rondGewichtAf", () => {
+//   expect(rondGewichtAf(0)).toBe(0);
+//   expect(rondGewichtAf(510)).toBe(500);
+//   expect(rondGewichtAf(550)).toBe(500);
+//   expect(rondGewichtAf(560)).toBe(600);
+//   expect(rondGewichtAf(1050)).toBe(1000);
+//   expect(rondGewichtAf(1051)).toBe(1100);
+// });
 
-test("findTarief", () => {
-  const tariefVanaf0 = {
-    threshold_kg: 0,
-    vast_euro: 10,
-  };
-  const tariefVanaf600 = {
-    threshold_kg: 600,
-    vast_euro: 20,
-  };
-  const tariefVanaf1000 = {
-    threshold_kg: 1000,
-    vast_euro: 30,
-  };
-  const tarieven = [tariefVanaf0, tariefVanaf600, tariefVanaf1000];
+// test("findTarief", () => {
+//   const tariefVanaf0 = {
+//     threshold_kg: 0,
+//     vast_euro: 10,
+//   };
+//   const tariefVanaf600 = {
+//     threshold_kg: 600,
+//     vast_euro: 20,
+//   };
+//   const tariefVanaf1000 = {
+//     threshold_kg: 1000,
+//     vast_euro: 30,
+//   };
+//   const tarieven = [tariefVanaf0, tariefVanaf600, tariefVanaf1000];
 
-  // tariefVanaf0
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 0,
-    })
-  ).toBe(tariefVanaf0);
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 500,
-    })
-  ).toBe(tariefVanaf0);
+//   // tariefVanaf0
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 0,
+//     })
+//   ).toBe(tariefVanaf0);
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 500,
+//     })
+//   ).toBe(tariefVanaf0);
 
-  // tariefVanaf600
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 600,
-    })
-  ).toBe(tariefVanaf600);
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 900,
-    })
-  ).toBe(tariefVanaf600);
+//   // tariefVanaf600
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 600,
+//     })
+//   ).toBe(tariefVanaf600);
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 900,
+//     })
+//   ).toBe(tariefVanaf600);
 
-  // tariefVanaf1000
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 1000,
-    })
-  ).toBe(tariefVanaf1000);
-  expect(
-    findTarief({
-      tarieven,
-      afgerondGewicht: 6000,
-    })
-  ).toBe(tariefVanaf1000);
+//   // tariefVanaf1000
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 1000,
+//     })
+//   ).toBe(tariefVanaf1000);
+//   expect(
+//     findTarief({
+//       tarieven,
+//       afgerondGewicht: 6000,
+//     })
+//   ).toBe(tariefVanaf1000);
 
-  // Only multiples of 100
-  expect(() => findTarief({ tarieven, afgerondGewicht: 99 })).toThrow();
-  expect(() => findTarief({ tarieven, afgerondGewicht: 100 })).not.toThrow();
-  expect(() => findTarief({ tarieven, afgerondGewicht: 101 })).toThrow();
-});
+//   // Only multiples of 100
+//   expect(() => findTarief({ tarieven, afgerondGewicht: 99 })).toThrow();
+//   expect(() => findTarief({ tarieven, afgerondGewicht: 100 })).not.toThrow();
+//   expect(() => findTarief({ tarieven, afgerondGewicht: 101 })).toThrow();
+// });
 
 test("Model_Personenauto/Benzine", () => {
   const tariefVanaf0 = {
@@ -96,8 +92,10 @@ test("Model_Personenauto/Benzine", () => {
   expect(
     Model_Personenauto({
       tarieven: {
-        [Brandstof.Benzine]: tarieven,
-        [Brandstof.Diesel]: [],
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: [],
+        },
       },
       gewicht: 0,
       brandstof: Brandstof.Benzine,
@@ -106,8 +104,10 @@ test("Model_Personenauto/Benzine", () => {
   expect(
     Model_Personenauto({
       tarieven: {
-        [Brandstof.Benzine]: tarieven,
-        [Brandstof.Diesel]: [],
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: [],
+        },
       },
       gewicht: 550,
       brandstof: Brandstof.Benzine,
@@ -116,8 +116,10 @@ test("Model_Personenauto/Benzine", () => {
   expect(
     Model_Personenauto({
       tarieven: {
-        [Brandstof.Benzine]: tarieven,
-        [Brandstof.Diesel]: [],
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: [],
+        },
       },
       gewicht: 650,
       brandstof: Brandstof.Benzine,
@@ -126,11 +128,94 @@ test("Model_Personenauto/Benzine", () => {
   expect(
     Model_Personenauto({
       tarieven: {
-        [Brandstof.Benzine]: tarieven,
-        [Brandstof.Diesel]: [],
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: [],
+        },
       },
       gewicht: 750,
       brandstof: Brandstof.Benzine,
     })
   ).toBe(21);
+});
+
+test("Model_Personenauto/Diesel", () => {
+  const tariefVanaf0 = {
+    threshold_kg: 0,
+    vast_euro: 10,
+  };
+  const tariefVanaf600 = {
+    threshold_kg: 600,
+    vast_euro: 20,
+    variabel: {
+      ondergrens: 600,
+      euro: 1,
+      per_kg: 100,
+    },
+  };
+  const tarieven = [tariefVanaf0, tariefVanaf600];
+
+  const toeslagVanaf0 = {
+    threshold_kg: 0,
+    vast_euro: 5,
+  };
+  const toeslagVanaf800 = {
+    threshold_kg: 800,
+    vast_euro: 10,
+    variabel: {
+      ondergrens: 800,
+      euro: 2.5,
+      per_kg: 100,
+    },
+  };
+  const toeslagen = [toeslagVanaf0, toeslagVanaf800];
+
+  expect(
+    Model_Personenauto({
+      tarieven: {
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: toeslagen,
+        },
+      },
+      gewicht: 0,
+      brandstof: Brandstof.Diesel,
+    })
+  ).toBe(15);
+  expect(
+    Model_Personenauto({
+      tarieven: {
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: toeslagen,
+        },
+      },
+      gewicht: 550,
+      brandstof: Brandstof.Diesel,
+    })
+  ).toBe(15);
+  expect(
+    Model_Personenauto({
+      tarieven: {
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: toeslagen,
+        },
+      },
+      gewicht: 850,
+      brandstof: Brandstof.Diesel,
+    })
+  ).toBe(32);
+  expect(
+    Model_Personenauto({
+      tarieven: {
+        basis: tarieven,
+        toeslagen: {
+          [Brandstof.Diesel]: toeslagen,
+        },
+      },
+      gewicht: 1050,
+      brandstof: Brandstof.Diesel,
+    })
+  ).toBe(39);
 });
